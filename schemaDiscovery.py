6f605a978f694f12c4a9aa854cd706d5657963a1
Lemonakis Georgios <glemon1@gmail.com>
2022-01-16 23:06:03 +0200

    on progress

--- schemaDiscovery.py
+++ schemaDiscovery.py
@@ -1,8 +1,11 @@
 from SPARQLWrapper import SPARQLWrapper, JSON, SPARQLWrapper2
+import json
+
+typePredicate= "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 
 sparql = SPARQLWrapper("http://83.212.77.24:8890/sparql/")
 
-dataSet = "Conference"#Conference"#"DBpedia"BNF
+dataSet = "BNF"#Conference DBpedia BNF
 #1. all trilets
 sparql.setQuery("""
     select ?s ?p ?o 
@@ -25,7 +28,7 @@ FILTER (?predicate!= <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>)
 order by ?predicate
 """)
 sparql.setReturnFormat(JSON)
-inputNN = sparql.query().convert()
+allPredicatesButType = sparql.query().convert()
 
 #3. types (NN output), machine learning output layer: all objects with predicate ‘type’ ordered. All types ordered. They are the outputs plus one for the ones not classified which will be processed in second phase
 sparql.setQuery("""
@@ -38,7 +41,7 @@ sparql.setQuery("""
     order by ?o
 """)
 sparql.setReturnFormat(JSON)
-outputNN = sparql.query().convert()
+allTypes = sparql.query().convert()
 
 #4. all subjects (instances)
 sparql.setQuery("""
@@ -53,9 +56,9 @@ sparql.setQuery("""
 sparql.setReturnFormat(JSON)
 instancesAll = sparql.query().convert()
 
-#5. all instances (subjects) having type property
+#5. all instances (subjects) having type property and their type
 sparql.setQuery("""
-    SELECT distinct ?s
+    SELECT distinct ?s ?o
     from <http://localhost:8890/""" + dataSet +""">
     WHERE
     {
@@ -64,32 +67,79 @@ sparql.setQuery("""
     order by ?s
 """)
 sparql.setReturnFormat(JSON)
-insstancesWithKnownTypes = sparql.query().convert()
+instancesWithKnownTypes = sparql.query().convert()
 
 
+instancesUnknown = []#[{}]
+inputNN = allPredicatesButType
+outputNN = allTypes
+
+#find instances with unknown type
 found = 'false'
-instancesUnknown = [{}]
-tripletsAll
-inputNN
-outputNN
-instancesAll
-insstancesWithKnownTypes
-
-# for triplet in tripletsAll["results"]["bindings"]:
-#     if tripletsAll['s']['value'] not in insstancesWithKnownTypes['s']:
-#         print (triplet['s']['value'])
-
 for instance in instancesAll["results"]["bindings"]:
-    for knownInstance in insstancesWithKnownTypes["results"]["bindings"]:
+    for knownInstance in instancesWithKnownTypes["results"]["bindings"]:
         if instance['s']['value'] == knownInstance['s']['value']:
             found = 'true'
             break
     if found == 'true': 
         found = 'false'
     else:
-        instancesUnknown.append(instance)
-        #print('unknown ' + instance['s']['value'])
-instancesUnknown   
+        instancesUnknown.append(instance['s']['value'])
+        print('unknown ' + instance['s']['value'])
+  
+#fix datasets
+
+inputTrainValues=[len(inputNN["results"]["bindings"])] #table with instance predicates 0,1
+outputTrainValues=[len(outputNN["results"]["bindings"])] #table with instance type 0,1
+
+input={}
+output = {}
+
+unknownSet=[]
+trainInput=[]
+trainOutput=[]
+unknownInput = []
+
+emptyInput = {}
+emptyOutput = {}
+for i in range (0, len(inputNN["results"]["bindings"])):
+    emptyInput[i]=0
+for i in range (0, len(outputNN["results"]["bindings"])):
+    emptyOutput[i]=0
+
+trainOutput.append(emptyOutput)
+trainInput.append(emptyInput)
+print('trainInput')
+print(trainInput)
+print('trainOutput')
+print(trainOutput)
+
+currentInstance = ""
+for triplet in tripletsAll["results"]["bindings"]:
+    if triplet['s']['value'] != currentInstance:#new instance tripletts
+        if currentInstance != "": #if not first element
+            trainOutput.append(output) #insert train data
+            trainInput.append(input)
+            output = emptyOutput     #new instance so put empty i/o tables
+            input = emptyInput
+            #next instance so insert previous in train set
+        currentInstance = triplet['s']['value']
+        for i in range (0, len(allPredicatesButType["results"]["bindings"])): #find predicates porition in nn input table data
+            found=-1
+            if triplet['p']['value'] == allPredicatesButType["results"]["bindings"][i]["predicate"]["type"]:
+                found = i
+                break
+        if found != -1: #position found
+            input[i]=1
+
+        if triplet['s']['value'] in instancesUnknown: # no type, unknown
+            dataSet
+
+   #     else: #put in train set
+            
+    #else: # same instance set triplets
+
+
 # for type in outputNN["results"]["bindings"]:
 #     print (type['o']['value'])
 
